77bc3d658395c88d05b2f657b4bfa70d4034adb0
R.Kiran Kumar <31879302+kirankumarhere@users.noreply.github.com>
2019-10-04 19:02:10 +0530

    Add files via upload

---
+++ Locks_critical_sections_omp-atomic/lock.c
@@ -0,0 +1,56 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INP_SIZE (1<<26)
+#define HIST_SIZE (1<<20)
+int hist[HIST_SIZE];
+int inp[INP_SIZE];
+
+omp_lock_t lock[HIST_SIZE]; //declaring locks//
+
+int main (int argc, char *argv[]) 
+{
+int i;
+int sum = 0,key;
+double t1,t2;
+
+//initialize locks..
+for( i=0;i<HIST_SIZE;i++)
+omp_init_lock(&lock[i]);
+
+for( i=0;i<HIST_SIZE;i++)
+hist[i]=0;
+
+for( i=0;i<INP_SIZE;i++)
+inp[i]=1;
+
+t1 = omp_get_wtime(); //start time...
+
+#pragma omp parallel for private(key)
+for(i=0;i<INP_SIZE;i++)
+{
+key = inp[i];
+
+//#pragma omp critical
+
+//we are trying to use this instead of critical sections..to reduce delays..
+omp_set_lock(&lock[key]);
+hist[key]++;
+omp_unset_lock(&lock[key]);
+
+}
+
+t2 = omp_get_wtime();
+
+for( i=0;i<HIST_SIZE;i++)
+sum+= hist[i];
+
+for( i=0;i<HIST_SIZE;i++)
+omp_destroy_lock(&lock[key]);
+
+printf("sum of array elements = %d, time: %g \n", sum,t2-t1);
+return 0;
+}
+
+
